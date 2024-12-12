caf98a4feec003705c129d44c0cdc05ab7368266
tanvipatil2102 <tanvi6829@gmail.com>
2024-12-12 20:33:21 +0530

    own task

---
+++ src/app/shared/components/products/products.component.ts
@@ -0,0 +1,181 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  prodCount : number = 1;
+
+  onMinusClick(){
+    if(this.prodCount > 1){
+      this.prodCount--
+    }
+  }
+
+  onAddBtn(){
+    if(this.prodCount < 10){
+      this.prodCount++
+    }
+  }
+  
+  products = [
+    {
+      albumId: 1,
+      id: 1,
+      title: "accusamus beatae ad facilis cum similique qui sunt",
+      url: "https://via.placeholder.com/600/92c952",
+      thumbnailUrl: "https://via.placeholder.com/150/92c952",
+      isAvailable : true,
+      rating : 4.6,
+      price :2500
+    },
+    {
+      albumId: 1,
+      id: 2,
+      title: "reprehenderit est deserunt velit ipsam",
+      url: "https://via.placeholder.com/600/771796",
+      thumbnailUrl: "https://via.placeholder.com/150/771796",
+      isAvailable : false,
+      rating : 4.2,
+      price :3500
+    },
+    {
+      albumId: 1,
+      id: 3,
+      title: "officia porro iure quia iusto qui ipsa ut modi",
+      url: "https://via.placeholder.com/600/24f355",
+      thumbnailUrl: "https://via.placeholder.com/150/24f355",
+      isAvailable : true,
+      rating : 3.6,
+      price :2000
+    },
+    {
+      albumId: 1,
+      id: 4,
+      title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+      url: "https://via.placeholder.com/600/d32776",
+      thumbnailUrl: "https://via.placeholder.com/150/d32776",
+      isAvailable : true,
+      rating : 0,
+      price :1000
+    },
+    {
+      albumId: 1,
+      id: 5,
+      title: "natus nisi omnis corporis facere molestiae rerum in",
+      url: "https://via.placeholder.com/600/f66b97",
+      thumbnailUrl: "https://via.placeholder.com/150/f66b97",
+      isAvailable : true,
+      rating : 1.5,
+      price :3000
+    },
+    {
+      albumId: 1,
+      id: 6,
+      title: "accusamus ea aliquid et amet sequi nemo",
+      url: "https://via.placeholder.com/600/56a8c2",
+      thumbnailUrl: "https://via.placeholder.com/150/56a8c2",
+      isAvailable : true,
+      rating : 4.6,
+      price :2500
+    },
+    {
+      albumId: 1,
+      id: 7,
+      title: "officia delectus consequatur vero aut veniam explicabo molestias",
+      url: "https://via.placeholder.com/600/b0f7cc",
+      thumbnailUrl: "https://via.placeholder.com/150/b0f7cc",
+      isAvailable : false,
+      rating : 4.3,
+      price :2500
+    },
+    {
+      albumId: 1,
+      id: 8,
+      title: "aut porro officiis laborum odit ea laudantium corporis",
+      url: "https://via.placeholder.com/600/54176f",
+      thumbnailUrl: "https://via.placeholder.com/150/54176f",
+      isAvailable : true,
+      rating : 1.6,
+      price :5500
+    },
+    {
+      albumId: 1,
+      id: 9,
+      title: "qui eius qui autem sed",
+      url: "https://via.placeholder.com/600/51aa97",
+      thumbnailUrl: "https://via.placeholder.com/150/51aa97",
+      isAvailable : true,
+      rating : 3.4,
+      price :6000
+    },
+    {
+      albumId: 1,
+      id: 10,
+      title: "beatae et provident et ut vel",
+      url: "https://via.placeholder.com/600/810b14",
+      thumbnailUrl: "https://via.placeholder.com/150/810b14",
+      isAvailable : true,
+      rating : 4.8,
+      price :2500
+    },
+    {
+      albumId: 1,
+      id: 11,
+      title: "nihil at amet non hic quia qui",
+      url: "https://via.placeholder.com/600/1ee8a4",
+      thumbnailUrl: "https://via.placeholder.com/150/1ee8a4",
+      isAvailable : false,
+      rating : 3.7,
+      price :2999    
+    },
+    {
+      albumId: 1,
+      id: 12,
+      title: "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
+      url: "https://via.placeholder.com/600/66b7d2",
+      thumbnailUrl: "https://via.placeholder.com/150/66b7d2",
+      isAvailable : true,
+      rating : 2.6,
+      price :3999
+    },
+    {
+      albumId: 1,
+      id: 13,
+      title: "repudiandae iusto deleniti rerum",
+      url: "https://via.placeholder.com/600/197d29",
+      thumbnailUrl: "https://via.placeholder.com/150/197d29",
+      isAvailable : true,
+      rating : 1.2,
+      price : 1500
+    },
+    {
+      albumId: 1,
+      id: 14,
+      title: "est necessitatibus architecto ut laborum",
+      url: "https://via.placeholder.com/600/61a65",
+      thumbnailUrl: "https://via.placeholder.com/150/61a65",
+      isAvailable : true,
+      rating : 2.7,
+      price :2500
+    },
+    {
+      albumId: 1,
+      id: 15,
+      title: "harum dicta similique quis dolore earum ex qui",
+      url: "https://via.placeholder.com/600/f9cee5",
+      thumbnailUrl: "https://via.placeholder.com/150/f9cee5",
+      isAvailable : false,
+      rating : 4.6,
+      price :4900
+    }
+  ]
+}
